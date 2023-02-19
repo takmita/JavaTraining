f058340ca2ca6b2b57271c0aa14e3d973493f113
takmita <mita@esm.co.jp>
2023-02-19 13:13:05 +0900

    練習問題6-2

---
+++ Code_6_2.java
@@ -0,0 +1,10 @@
+public class Code_6_2 {
+    public static void main(String[] args) {
+        int i = 3;
+        int j = 6;
+        System.out.println(i + "と" +j + "の平均は" + ave(i, j) + "です");
+    }
+    public static  int ave(int i,int j) {
+        return  (i + j) / 2;
+    }
+}
