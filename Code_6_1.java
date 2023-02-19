6d07cc6f75a2ef72b0a806ebae4021fbb962926f
takmita <mita@esm.co.jp>
2023-02-19 12:31:10 +0900

    練習問題6-1

---
+++ Code_6_1.java
@@ -0,0 +1,9 @@
+public class Code_6_1 {
+    public static void main(String[] args) {
+        int i = 3;
+        System.out.println(i + "の２乗は" + pow2(i) + "です");
+    }
+    public static  int pow2(int i){
+        return  i * i;
+    }
+}
